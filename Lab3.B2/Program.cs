5d92259e2a93a3a4b7ac791261682257c7b8c00c
kinghia <162483327+kinghia@users.noreply.github.com>
2024-05-24 16:23:49 +0700

    load lan dau

---
+++ Lab3.B2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    private static string content;
+    static void Data()
+    {
+        string username = "Nghĩa";
+        string password = "50124141";
+        string time = DateTime.Now.ToString();
+
+        using (StringWriter sw = new StringWriter())
+        {
+            string content = sw.ToString();
+
+            sw.WriteLine("UserName: " + username);
+            sw.WriteLine("PassWord: " + password);
+
+            Console.WriteLine(content);
+        }
+        using (StringReader sr = new StringReader(content))
+        {
+
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+    }
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Data();
+    }
+    
+}
---
+++ Lab3.B3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.B3
+{
+    internal class Program
+    {
+        static void create()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string dataDirectory = @"C:\Users\Huu Nghia\Desktop\C#2GAM109\Demo\Bai3Lab3";
+            Directory.CreateDirectory(dataDirectory);
+            string dataFilePath = Path.Combine(dataDirectory, "data.txt");
+
+            using (StreamWriter writer = new StreamWriter(dataFilePath))
+            {
+                writer.WriteLine("Mssv: PD11095");
+                writer.WriteLine("Họ và tên: Nguyễn Văn Hữu Nghĩa");
+            }
+            Console.WriteLine("Đã ghi xong: " + dataFilePath);
+        }
+        static void copy()
+        {
+            string sourceDirectory = @"..\\..\\..\data\\";
+            string targetDirectory = @"..\\..\\..\data2\\";
+
+            if (Directory.Exists(sourceDirectory))
+            {
+                Directory.CreateDirectory(targetDirectory);
+                string[] files = Directory.GetFiles(sourceDirectory);
+                foreach (string file in files)
+                {
+                    string fileName = Path.GetFileName(file);
+                    string targetFile = Path.Combine(targetDirectory, fileName);
+                    File.Copy(file, targetFile, true);
+                }
+                    Console.WriteLine("Sao chép hoàn tất");
+            }
+            else
+            {
+                Console.WriteLine("Thư mục nguồn không tồn tại");              
+            }
+        }
+        static void Main()
+        {
+            create();
+            copy();
+        }
+    }
+}
